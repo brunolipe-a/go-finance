c43fdad18d2108396a90a35659d586784b3b17e5
Bruno Felipe <bfaa1999@gmail.com>
2021-02-15 23:18:15 -0300

    ep02

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <div>
+      <header>
+        <span>Go Finance</span>
+        <nav>
+          <a href="/">Listagem</a>
+          <a href="/create">Cadastro</a>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
